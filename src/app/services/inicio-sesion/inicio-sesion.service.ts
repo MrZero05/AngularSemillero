d6ca6946ca3d30c8070d5151cda1bb6c0f1ed4fe
Jose Daniel Osorio Morales <josorio@personalsoft.com.co>
2020-01-29 18:33:09 -0500

    Se crea servicio con explicación de constructor.

--- src/app/services/inicio-sesion/inicio-sesion.service.ts
+++ src/app/services/inicio-sesion/inicio-sesion.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InicioSesionService {
 
-  constructor() { }
+   // Constructor por defecto sirve asi
+  /**constructor(http: HttpClient) {
+    this.http = http;
+  }**/
+
+  constructor(private http: HttpClient){}
+
+  iniciarSesion() {
+    this.http.get('');
+  }
 }
